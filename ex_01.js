33d9cd574baa136ebcfcdb5411e6a8bd5e204247
AldricGarcia <aldric.garcia@epitech.digital>
2024-10-02 12:22:40 +0200

    firstcommit

---
+++ ex_01.js
@@ -0,0 +1,9 @@
+function getAngryDog(numberOfWoofs)
+{
+    let result = "woof";
+    for (let i = 1; i < numberOfWoofs; i++)
+    {
+  	    result += " woof";
+    }
+    return result;
+}
---
+++ ex_03.js
@@ -0,0 +1,21 @@
+function fillBus(busStops, busSteats)
+{
+    let totalPeople = 0;
+    let index = 0;
+    if(!Array.isArray(busStops) || typeof busSteats !== 'number' || busSteats < 0)
+    {
+        return -1;
+    }
+    while(index < busStops.length)
+    {
+        totalPeople += busStops[index];
+        if(totalPeople >= busSteats)
+        {
+            return index + 1;
+        }
+        index++;
+    }
+    return -1;
+}
+
+console.log(fillBus([1, 3, 10, 1], 12));
